ae4e37c400a3f6b22f127c9816343226e71a7df2
Daniel Murphy <rdanielmurphy@gmail.com>
2019-03-09 19:22:54 -0500

    Added AWS stuff

---
+++ src/data/aws/awsCards.js
@@ -0,0 +1,19 @@
+import awsfc1 from "./question1.fc";
+import awsfc2 from "./question2.fc";
+import awsfc3 from "./question3.fc";
+import awsfc4 from "./question4.fc";
+import awsfc5 from "./question5.fc";
+import awsfc6 from "./question6.fc";
+
+const awsCards = {
+    questions: [
+        awsfc1,
+        awsfc2,
+        awsfc3,
+        awsfc4,
+        awsfc5,
+        awsfc6
+    ]
+};
+
+export default awsCards;
--- src/data/flashcard.data.js
+++ src/data/flashcard.data.js
@@ -5,42 +5,48 @@ import javaCards from "./java/javaCards";
 import greCards from "./gre/greCards";
 import puzzleCards from "./puzzles/puzzleCards";
 import htmlcssCards from "./htmlcss/htmlcssCards";
+import awsCards from "./aws/awsCards";
 
 const FlashCardData = {
-    "js" :  {
-        "name" : "JavaScript",
-        "cards" : jsCards,
-        "fa" : "code"
+    "js": {
+        "name": "JavaScript",
+        "cards": jsCards,
+        "fa": "code"
     },
-    "htmlcss" : {
-        "name" : "HTML/CSS",
-        "cards" : htmlcssCards,
-        "fa" : "html5"
+    "htmlcss": {
+        "name": "HTML/CSS",
+        "cards": htmlcssCards,
+        "fa": "html5"
     },
-    "blockchain" :  {
-        "name" : "Blockchain",
-        "cards" : blockchainCards,
-        "fa" : "bitcoin"
+    "blockchain": {
+        "name": "Blockchain",
+        "cards": blockchainCards,
+        "fa": "bitcoin"
     },
-    "theory" :  {
-        "name" : "Theory",
-        "cards" : theoryCards,
-        "fa" : "linux"
+    "theory": {
+        "name": "Theory",
+        "cards": theoryCards,
+        "fa": "linux"
     },
-    "java" :  {
-        "name" : "Java",
-        "cards" : javaCards,
-        "fa" : "terminal"
+    "java": {
+        "name": "Java",
+        "cards": javaCards,
+        "fa": "terminal"
     },
-    "gre" :  {
-        "name" : "GRE Vocab",
-        "cards" : greCards,
-        "fa" : "font"
+    "gre": {
+        "name": "GRE Vocab",
+        "cards": greCards,
+        "fa": "font"
     },
-    "puzzles" : {
-        "name" : "Puzzles",
-        "cards" : puzzleCards,
-        "fa" : "puzzle-piece"
+    "puzzles": {
+        "name": "Puzzles",
+        "cards": puzzleCards,
+        "fa": "puzzle-piece"
+    },
+    "aws": {
+        "name": "AWS",
+        "cards": awsCards,
+        "fa": "aws"
     }
 };
 
